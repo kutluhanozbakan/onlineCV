c6bd12e1340ddd13756105bd13b1bc468ffde0ea
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2021-08-05 13:04:18 +0300

    Truncate Pipe Kurulumu

--- onlineCV-app/src/app/app.module.ts
+++ onlineCV-app/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//JSON İÇİN EKLENTİ
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContainerComponent } from './container/container.component';
+import { TruncatePipePipe } from './container/truncate-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContainerComponent
+    ContainerComponent,
+    TruncatePipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- onlineCV-app/src/app/container/container.component.ts
+++ onlineCV-app/src/app/container/container.component.ts
@@ -2,6 +2,7 @@ import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from '../services/alertify.service';
 import { egitimBilgileri } from './container';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -12,18 +13,24 @@ import { egitimBilgileri } from './container';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor(private alertify: AlertifyService) { }
+  constructor(private alertify: AlertifyService,
+              private http: HttpClient) { }
+  path = "http://localhost:3000/egitimler"
 
   egitimBilgileri: egitimBilgileri[] =
   [
-    {id: 1, adi: "Javascript", kategoriID: 1, aciklama: "Aciklama1", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
-    {id: 2, adi: "TypeScript", kategoriID: 1, aciklama: "Aciklama2", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
-    {id: 3, adi: "Angular", kategoriID: 1, aciklama: "Aciklama3", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
-    {id: 4, adi: "HTML", kategoriID: 1, aciklama: "Aciklama4", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
-    {id: 5, adi: "CSS", kategoriID: 1, aciklama: "Aciklama5", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
-  ]
+  //   {id: 1, adi: "Javascript", kategoriID: 1, aciklama: "Aciklama1", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
+  //   {id: 2, adi: "TypeScript", kategoriID: 1, aciklama: "Aciklama2", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
+  //   {id: 3, adi: "Angular", kategoriID: 1, aciklama: "Aciklama3", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
+  //   {id: 4, adi: "HTML", kategoriID: 1, aciklama: "Aciklama4", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
+  //   {id: 5, adi: "CSS", kategoriID: 1, aciklama: "Aciklama5", imageUrl: "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"},
+]
 
   ngOnInit(): void {
+    //JSON VERİLERİNİ EKLEME
+    this.http.get<egitimBilgileri[]>(this.path).subscribe(data => {
+      this.egitimBilgileri = data
+    })
   }
   addtoChart(egitimAdi: any)
   {
